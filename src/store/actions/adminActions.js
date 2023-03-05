f34c171581a5420dba0e15a4f0c69076307105ff
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-03-05 18:15:27 +0700

    commit create user

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,5 +1,5 @@
 import actionTypes from './actionTypes';
-import { getAllCodeService } from '../../services/userService';
+import { getAllCodeService, createNewUserService } from '../../services/userService';
 
 
 // export const fetchGenderStart = () => ({
@@ -83,5 +83,28 @@ export const fetchRoleStart = () => {
         }
     }
 }
+
+export const createNewUser = (data) => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await createNewUserService(data);
+            if (res && res.errCode === 0) {
+                dispatch(saveUserSuccess(res.data));
+            } else {
+                dispatch(saveUserFailed());
+            }
+        } catch (e) {
+            dispatch(saveUserFailed());
+            console.log(e)
+        }
+    }
+}
+
+export const saveUserSuccess = () => ({
+    type: 'CREATE_USER_SUCCESS'
+})
+export const saveUserFailed = () => ({
+    type: 'CREATE_USER_SUCCESS'
+})
 
 
